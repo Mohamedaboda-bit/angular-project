21ef62638f06ab0a9da0e6ea87499484418d8f60
Rola Alaa <rolaalaa670>
2025-06-11 14:14:13 +0300

    Edit Profile

--- src/app/modules/admin/pages/profile/profile.component.ts
+++ src/app/modules/admin/pages/profile/profile.component.ts
@@ -13,8 +13,8 @@ export class ProfileComponent {
   adminProfile = {
     name: 'Ahmed Mohamed',
     role: 'Administrator',
-    email: 'ahmed.mohamed@example.com',
+    email: 'admin2@example.com',
     joinDate: '2024-01-01',
-    profileImage: '../../../../assets/profile.jpg'
+    profileImage: 'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D'
   };
 } 
